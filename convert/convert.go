e40df675a76bfa1a4ddd9d8883e1ed6c6b52da4a
Steve Yen <steve.yen@gmail.com>
2016-03-31 13:16:11 -0700

    track conversions

--- convert/convert.go
+++ convert/convert.go
@@ -10,7 +10,7 @@
 //  governing permissions and limitations under the License.
 
 // Package convert provides tooling API's to instrument go code with
-// calls to gapture's runtime API.
+// calls to the runtime API.
 package convert
 
 import (
@@ -23,9 +23,10 @@ import (
 	"go/types"
 	"os"
 	"reflect"
+	"strings"
 )
 
-var GapturePackageName = "github.com/couchbaselabs/gapture"
+var RuntimePackageName = "github.com/couchbaselabs/gapture"
 
 // Options allows users to override the default behavior of the
 // instrumentation processing.
@@ -38,15 +39,15 @@ type Options struct {
 	Logf    func(fmt string, v ...interface{})
 }
 
-var GaptureStackAssignStmt *ast.AssignStmt
+var RuntimeStackAssignStmt *ast.AssignStmt
 
 func init() {
-	expr, err := parser.ParseExpr(`func() { gaptureGID, gaptureStack := gapture.Stack(0) }`)
+	expr, err :=
+		parser.ParseExpr(`func() { gaptureGID, gaptureStack := gapture.Stack(0) }`)
 	if err != nil {
 		panic(err)
 	}
-	GaptureStackAssignStmt = expr.(*ast.FuncLit).Body.List[0].(*ast.AssignStmt)
-	fmt.Printf("GaptureStackAssignStmt: %#v\n", GaptureStackAssignStmt)
+	RuntimeStackAssignStmt = expr.(*ast.FuncLit).Body.List[0].(*ast.AssignStmt)
 }
 
 func ProcessDirs(paths []string, options Options) error {
@@ -97,10 +98,20 @@ func ProcessDirs(paths []string, options Options) error {
 			logf("types.config.Check(): %s => %v\n", pkg.Name, pkgChecked)
 
 			for fileName, file := range pkg.Files {
-				// If the file defines func's, then add import of
-				// gapture package, if not already.
-				if FileHasFuncs(file) &&
-					!FileImportsPackage(file, GapturePackageName) {
+				converter := &Converter{
+					options:  &options,
+					info:     info,
+					fileName: fileName,
+					file:     file,
+					node:     file,
+				}
+
+				ast.Walk(converter, file)
+
+				// If the file had conversions, then add import of the
+				// runtime package, if not already.
+				if converter.conversions > 0 &&
+					!FileImportsPackage(file, RuntimePackageName) {
 					file.Decls = append([]ast.Decl{
 						&ast.GenDecl{
 							Tok: token.IMPORT,
@@ -108,21 +119,13 @@ func ProcessDirs(paths []string, options Options) error {
 								&ast.ImportSpec{
 									Path: &ast.BasicLit{
 										Kind:  token.STRING,
-										Value: `"` + GapturePackageName + `"`,
+										Value: `"` + RuntimePackageName + `"`,
 									},
 								},
 							},
 						},
 					}, file.Decls...)
 				}
-
-				ast.Walk(&Converter{
-					options:  &options,
-					info:     info,
-					fileName: fileName,
-					file:     file,
-					node:     file,
-				}, file)
 
 				err = format.Node(os.Stdout, fileSet, file)
 				if err != nil {
@@ -152,43 +155,73 @@ func FileImportsPackage(file *ast.File, pkgName string) bool {
 	return false
 }
 
-func FileHasFuncs(file *ast.File) bool {
-	for _, decl := range file.Decls {
-		_, ok := decl.(*ast.FuncDecl)
-		if ok {
-			return true
-		}
-	}
+// UsesChannels returns true if the ast.Node uses channels.
+func UsesChannels(info *types.Info, topNode ast.Node) bool {
+	rv := false
 
-	return false
+	ast.Inspect(topNode, func(node ast.Node) bool {
+		switch x := node.(type) {
+		case *ast.SelectStmt:
+			rv = true
+		case *ast.SendStmt:
+			rv = true
+		case *ast.UnaryExpr:
+			if x.Op == token.ARROW {
+				rv = true
+			}
+		case *ast.RangeStmt:
+			t := info.TypeOf(x.X)
+			rv = rv || strings.HasPrefix(t.String(), "chan ")
+		}
+
+		return rv == false
+	})
+
+	return rv
 }
 
 // ----------------------------------------------------------------
 
 type Converter struct {
+	parent   *Converter
 	options  *Options
 	info     *types.Info
 	fileName string
 	file     *ast.File
 	node     ast.Node
-	parent   *Converter
+
+	conversions int
+}
+
+func (v *Converter) MarkConversion() {
+	if v == nil {
+		return
+	}
+
+	v.conversions++
+	v.parent.MarkConversion()
 }
 
 func (v *Converter) Visit(node ast.Node) ast.Visitor {
 	if node != nil {
 		fmt.Printf("%s ", v.fileName)
 
-		for vv := v; vv != nil; vv = vv.parent {
+		for vv := v; vv != nil; vv = vv.parent { // Indentation by depth.
 			fmt.Print(" ")
 		}
 
 		fmt.Printf("%s", reflect.TypeOf(node).String())
 
 		switch x := node.(type) {
-		case *ast.File:
-			fmt.Printf(" name: %v", x.Name)
 		case *ast.FuncDecl:
 			fmt.Printf(" name: %v", x.Name)
+			if UsesChannels(v.info, x) {
+				fmt.Printf(" uses-channels")
+			}
+		case *ast.FuncLit:
+			if UsesChannels(v.info, x) {
+				fmt.Printf(" uses-channels")
+			}
 		case *ast.BasicLit:
 			fmt.Printf(" value: %v", x.Value)
 		case ast.Expr:
@@ -202,11 +235,11 @@ func (v *Converter) Visit(node ast.Node) ast.Visitor {
 	}
 
 	return &Converter{
+		parent:   v,
 		options:  v.options,
 		info:     v.info,
 		fileName: v.fileName,
 		file:     v.file,
 		node:     node,
-		parent:   v,
 	}
 }
--- example.go
+++ example.go
@@ -6,4 +6,18 @@ import (
 
 func Example() {
 	fmt.Printf("an example function, useful to test convert processing")
+
+	f := func(x int) int { return x + 1 }
+
+	f(1)
+}
+
+func ExampleWithChan() {
+	ch := make(chan bool)
+	close(ch)
+
+	rv := false
+	for t := range ch {
+		rv = rv || t
+	}
 }
