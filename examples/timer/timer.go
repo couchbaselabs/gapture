7c833f0bc56424ed3321f2ed6ac927c92ed2a4e7
Steve Yen <steve.yen@gmail.com>
2016-04-08 22:22:06 -0700

    added timer example

---
+++ examples/timer/timer.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+
+func timer(d time.Duration) <-chan int {
+    c := make(chan int)
+    go func() {
+        time.Sleep(d)
+        c <- 1
+    }()
+    return c
+}
+
+func main() {
+    for i := 0; i < 24; i++ {
+        c := timer(1 * time.Second)
+        <-c
+    }
+}
