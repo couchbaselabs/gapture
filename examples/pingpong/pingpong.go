4d010347c9d458601f7b7e1a40fda5da1c57a5f1
Steve Yen <steve.yen@gmail.com>
2016-04-08 22:30:40 -0700

    fan in example

--- examples/pingpong/pingpong.go
+++ examples/pingpong/pingpong.go
@@ -3,22 +3,22 @@ package main
 import "time"
 
 func main() {
-    var Ball int
+	var Ball int
 
-    table := make(chan int)
-    go player(table)
-    go player(table)
+	table := make(chan int)
+	go player(table)
+	go player(table)
 
-    table <- Ball
-    time.Sleep(1 * time.Second)
-    <-table
+	table <- Ball
+	time.Sleep(1 * time.Second)
+	<-table
 }
 
 func player(table chan int) {
-    for {
-        ball := <-table
-        ball++
-        time.Sleep(100 * time.Millisecond)
-        table <- ball
-    }
+	for {
+		ball := <-table
+		ball++
+		time.Sleep(100 * time.Millisecond)
+		table <- ball
+	}
 }
