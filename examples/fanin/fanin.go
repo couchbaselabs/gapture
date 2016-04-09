487d0bed69b9b9874ff58dcb9505118146d8f5ba
Steve Yen <steve.yen@gmail.com>
2016-04-08 22:33:12 -0700

    fan out example

--- examples/fanin/fanin.go
+++ examples/fanin/fanin.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func producer(ch chan int, d time.Duration) {
-    var i int
-    for {
-        ch <- i
-        i++
-        time.Sleep(d)
-    }
+	var i int
+	for {
+		ch <- i
+		i++
+		time.Sleep(d)
+	}
 }
 
 func reader(out chan int) {
-    for x := range out {
-        fmt.Println(x)
-    }
+	for x := range out {
+		fmt.Println(x)
+	}
 }
 
 func main() {
-    ch := make(chan int)
-    out := make(chan int)
-    go producer(ch, 100*time.Millisecond)
-    go producer(ch, 250*time.Millisecond)
-    go reader(out)
-    for i := range ch {
-        out <- i
-    }
+	ch := make(chan int)
+	out := make(chan int)
+	go producer(ch, 100*time.Millisecond)
+	go producer(ch, 250*time.Millisecond)
+	go reader(out)
+	for i := range ch {
+		out <- i
+	}
 }
